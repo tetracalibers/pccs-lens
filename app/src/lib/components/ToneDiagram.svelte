110a3e3c6846863ce508363439d675dbd8966797
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-03-09 18:13:40 +0900

    ハイライトされているトーンの自動コントラスト

--- app/src/lib/components/ToneDiagram.svelte
+++ app/src/lib/components/ToneDiagram.svelte
@@ -158,8 +158,8 @@
       {@const strokeWidth = isUsed ? 1.5 : 1}
       {@const labelFill = isUsed
         ? isLightColor(cell.usedColors[0].hex)
-          ? "#333"
-          : "#fff"
+          ? `oklch(from ${cell.usedColors[0].hex} calc(l - .60) c h)`
+          : `oklch(from ${cell.usedColors[0].hex} calc(l + .60) c h);`
         : isSCell && isCard199
           ? "#bbb"
           : "#aaa"}
@@ -204,9 +204,8 @@
           y={cell.cy}
           text-anchor="middle"
           dominant-baseline="central"
-          font-size="10"
-          fill={labelFill}
-          style="pointer-events: none; user-select: none;">{cell.label}</text
+          font-size={isUsed ? 11 : 10}
+          style={`pointer-events: none; user-select: none; fill: ${labelFill};`}>{cell.label}</text
         >
       </g>
     {/each}
