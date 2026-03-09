c9e17dfcf858aaa99afad8ab5d5f233a94549579
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-03-09 15:19:39 +0900

    iconifyを導入し、iconify経由でアイコンを表示

--- app/src/lib/components/ColorEntryItem.svelte
+++ app/src/lib/components/ColorEntryItem.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import ColorPicker from "$lib/components/ColorPicker.svelte"
   import type { PCCSColor } from "$lib/data/types"
+  import Icon from "@iconify/svelte"
 
   let {
     inputHex = $bindable(),
@@ -28,15 +29,7 @@
   <div class="remove-col">
     {#if showRemove}
       <button class="remove-btn" onclick={onRemove} aria-label="この色を削除">
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-          <path
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            d="M9.5 14.5v-3m5 3v-3M3 6.5h18c-1.404 0-2.107 0-2.611.337a2 2 0 0 0-.552.552C17.5 7.893 17.5 8.596 17.5 10v5.5c0 1.886 0 2.828-.586 3.414s-1.528.586-3.414.586h-3c-1.886 0-2.828 0-3.414-.586S6.5 17.386 6.5 15.5V10c0-1.404 0-2.107-.337-2.611a2 2 0 0 0-.552-.552C5.107 6.5 4.404 6.5 3 6.5Zm6.5-3s.5-1 2.5-1s2.5 1 2.5 1"
-            stroke-width="1.5"
-          />
-        </svg>
+        <Icon icon="lets-icons:trash-light" />
       </button>
     {/if}
   </div>
@@ -96,6 +89,9 @@
     display: flex;
     align-items: center;
     justify-content: center;
+  }
+  .remove-btn :global(svg) {
+    stroke-width: 1.5;
   }
 
   .remove-btn:hover {
