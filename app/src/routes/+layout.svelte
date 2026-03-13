275300bb941d8e050d83a3dcdc3759941684719a
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-03-13 09:45:44 +0900

    Safariで文字が太く潰れて見える問題を修正

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -11,7 +11,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
   <link
-    href="https://fonts.googleapis.com/css2?family=Reddit+Mono:wght@200..900&family=SUSE+Mono:ital@0;1&family=Zen+Kaku+Gothic+Antique&display=swap"
+    href="https://fonts.googleapis.com/css2?family=Reddit+Mono:wght@400..700&family=SUSE+Mono:wght@400;500&family=Zen+Kaku+Gothic+Antique:wght@400;500&display=swap"
     rel="stylesheet"
   />
 </svelte:head>
@@ -45,7 +45,9 @@
   :global(body) {
     --font-mono: "Reddit Mono", monospace;
 
-    font-family: "SUSE Mono", "Zen Kaku Gothic Antique";
+    font-family: "SUSE Mono", "Zen Kaku Gothic Antique", sans-serif;
+    font-synthesis-weight: none;
+
     margin-block: 0;
   }
 
