6048f1209f4831511781954ed7fc1e5baaa86628
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-03-13 17:46:15 +0900

    サジェスト制限を目視で微調整

--- app/src/lib/patterns/checkerboard-rules.ts
+++ app/src/lib/patterns/checkerboard-rules.ts
@@ -11,19 +11,19 @@ export const CHECKERBOARD_RULES: Record<ThemeId, CheckerboardRule> = {
     assort: { suggestedHues: [24, 1], suggestedTones: ["lt"] }
   },
   casual: {
-    base: { suggestedHues: [4, 5, 6], suggestedTones: ["lt"] },
-    assort: { suggestedHues: [10, 11, 12, 13, 14, 15, 16, 17, 18], suggestedTones: ["b"] }
+    base: { suggestedHues: [4, 5, 6], suggestedTones: ["p"] },
+    assort: { suggestedHues: [10, 11, 12, 13, 14], suggestedTones: ["b"] }
   },
   classic: {
-    base: { suggestedHues: [1, 2, 3, 4, 5, 6], suggestedTones: ["dk", "dkg"] },
-    assort: { suggestedHues: [11, 12, 13, 14, 15, 16], suggestedTones: ["dp"] }
+    base: { suggestedHues: [1, 2, 3], suggestedTones: ["dk", "dkg"] },
+    assort: { suggestedHues: [4, 5, 6], suggestedTones: ["g", "dk"] }
   },
   clear: {
-    base: { suggestedHues: [14, 15, 16, 17, 18, 19], suggestedTones: ["lt", "p"] },
+    base: { suggestedHues: [14, 15, 16, 17, 18], suggestedTones: ["lt", "p"] },
     assort: { suggestedHues: [], suggestedTones: ["W"] }
   },
   chic: {
-    base: { suggestedHues: [], suggestedTones: ["dkGy"] },
+    base: { suggestedHues: [], suggestedTones: ["dkGy", "mGy"] },
     assort: {
       suggestedHues: [20, 21, 22, 23, 24, 1, 2, 3, 8, 9, 10, 11, 12, 13, 14, 15, 16],
       suggestedTones: ["g", "dk"]
@@ -34,11 +34,11 @@ export const CHECKERBOARD_RULES: Record<ThemeId, CheckerboardRule> = {
     assort: { suggestedHues: [2, 5, 8, 12, 18], suggestedTones: ["b", "s", "v"] }
   },
   "warm-natural": {
-    base: { suggestedHues: [4, 5, 6, 7], suggestedTones: ["sf", "d"] },
-    assort: { suggestedHues: [10, 11, 12], suggestedTones: ["ltg", "sf", "d"] }
+    base: { suggestedHues: [4, 5, 6], suggestedTones: ["ltg"] },
+    assort: { suggestedHues: [10, 11, 12], suggestedTones: ["d"] }
   },
   "fresh-natural": {
-    base: { suggestedHues: [9, 10, 11, 12, 13, 14, 15, 16], suggestedTones: ["lt", "p"] },
+    base: { suggestedHues: [10, 11, 12, 13, 14, 15, 16], suggestedTones: ["lt", "p"] },
     assort: { suggestedHues: [], suggestedTones: ["W"] }
   },
   modern: {
@@ -47,6 +47,6 @@ export const CHECKERBOARD_RULES: Record<ThemeId, CheckerboardRule> = {
   },
   romantic: {
     base: { suggestedHues: [24, 1], suggestedTones: ["p"] },
-    assort: { suggestedHues: [3, 4], suggestedTones: ["lt"] }
+    assort: { suggestedHues: [4], suggestedTones: ["p"] }
   }
 }
