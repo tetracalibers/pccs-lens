c5c52ea80c0b79c7ffe58578adba1d395813c924
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-03-19 19:19:36 +0900

    wrangler setup [skip ci]

--- app/src/app.d.ts
+++ app/src/app.d.ts
@@ -2,12 +2,18 @@
 // for information about these interfaces
 declare global {
   namespace App {
+    interface Platform {
+      env: Env;
+      ctx: ExecutionContext;
+      caches: CacheStorage;
+      cf?: IncomingRequestCfProperties
+    }
+
     // interface Error {}
     // interface Locals {}
     // interface PageData {}
     // interface PageState {}
-    // interface Platform {}
   }
 }
 
-export {}
+export {};
