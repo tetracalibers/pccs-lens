5b91a8f26da1543088572bd8aba3a1684c6bfc74
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-03-19 21:48:57 +0900

    sv add mdsvex

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,3 +1,4 @@
+import { mdsvex } from "mdsvex"
 import adapter from "@sveltejs/adapter-static"
 
 const isGithubPages = process.env.GITHUB_PAGES === "true"
@@ -5,13 +6,11 @@ const isGithubPages = process.env.GITHUB_PAGES === "true"
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      fallback: "404.html"
-    }),
-    paths: {
-      base: isGithubPages ? "/pccs-lens" : ""
-    }
-  }
+    adapter: adapter({ fallback: "404.html" }),
+    paths: { base: isGithubPages ? "/pccs-lens" : "" }
+  },
+  preprocess: [mdsvex()],
+  extensions: [".svelte", ".svx"]
 }
 
 export default config
