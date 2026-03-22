14e7d41e2333509f79ef2f38e03990ef580ce14f
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-03-22 22:41:12 +0900

    より太いweightのフォントを使用できるように

--- proto/ui-patterns/src/routes/+layout.svelte
+++ proto/ui-patterns/src/routes/+layout.svelte
@@ -9,7 +9,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
   <link
-    href="https://fonts.googleapis.com/css2?family=Reddit+Mono:wght@400..700&family=SUSE+Mono:wght@400;500&family=Zen+Kaku+Gothic+Antique:wght@400;500&display=swap"
+    href="https://fonts.googleapis.com/css2?family=Reddit+Mono&family=SUSE+Mono&family=Zen+Kaku+Gothic+Antique:wght@400;500&display=swap"
     rel="stylesheet"
   />
 </svelte:head>
@@ -21,6 +21,5 @@
     --font-mono: "Reddit Mono", monospace;
 
     font-family: "SUSE Mono", "Zen Kaku Gothic Antique", sans-serif;
-    font-synthesis-weight: none;
   }
 </style>
