7227f09528e56c130d300cbb8a28c11cddff09e7
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-04-12 06:33:10 +0900

    三原色プレビューを表示

---
+++ app/src/lib/demo/PCCSColor.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { isLightColor } from "$lib/color/utils"
+  import { PCCS_MAP } from "$lib/data/pccs"
+
+  let { pccs }: { pccs: string } = $props()
+
+  const color = $derived(PCCS_MAP.get(pccs)!)
+  const isLightBg = $derived(isLightColor(color))
+</script>
+
+<div class="square" style:--_pccs-color={color} style:--_text-color={isLightBg ? "black" : "white"}>
+  {pccs}
+</div>
+
+<style>
+  .square {
+    --_square-size: 3rem;
+
+    width: var(--_square-size);
+    height: var(--_square-size);
+    color: var(--_text-color);
+    background-color: var(--_pccs-color);
+    display: grid;
+    place-items: center;
+    font-family: var(--font-mono);
+  }
+</style>
---
+++ app/src/lib/demo/color-mixing/PrimaryColorsRGB.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import PrimaryColors from "./PrimaryColors.svelte"
+
+  const PCCS_RGB_LIST = [
+    { pccs: "v3", name: "R" },
+    { pccs: "v12", name: "G" },
+    { pccs: "v19", name: "B" }
+  ]
+</script>
+
+<PrimaryColors colors={PCCS_RGB_LIST} />
