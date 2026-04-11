92a126abbb15c463cc630e38fa63718f483ee466
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-04-12 08:47:15 +0900

    SVGWrapperで最大幅を指定できるように

--- app/src/lib/demo/SVGWrapper.svelte
+++ app/src/lib/demo/SVGWrapper.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import type { Snippet } from "svelte"
 
-  let { children }: { children: Snippet } = $props()
+  let { children, maxW }: { children: Snippet; maxW?: number } = $props()
 </script>
 
-<div class="svg-wrapper">
+<div class="svg-wrapper" style:--_max-w={maxW ? `${maxW}px` : "none"}>
   {@render children()}
 </div>
 
@@ -19,5 +19,7 @@
     height: auto;
     padding-block: 1rem;
     box-sizing: border-box;
+    max-width: var(--_max-w);
+    margin-inline: auto;
   }
 </style>
