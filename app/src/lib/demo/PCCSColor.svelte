9dc2f1bf7993adc80ad590dcc360ee5d9035c959
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-04-14 16:11:08 +0900

    WトーンやBkトーンのダークモード対応

--- app/src/lib/demo/PCCSColor.svelte
+++ app/src/lib/demo/PCCSColor.svelte
@@ -12,7 +12,8 @@
   class="square"
   style:--_pccs-color={color}
   style:--_text-color={isLightBg ? "black" : "white"}
-  class:--_need-border={pccs === "W"}
+  class:--_white-tone={pccs === "W"}
+  class:--_black-tone={pccs === "Bk"}
 >
   {pccs}
 </div>
@@ -20,6 +21,8 @@
 <style>
   .square {
     --_square-size: 3rem;
+    --_black-border: rgba(0, 0, 0, 0.2);
+    --_white-border: rgba(255, 255, 255, 0.2);
 
     width: var(--_square-size);
     height: var(--_square-size);
@@ -30,10 +33,13 @@
     font-family: var(--font-mono);
     font-size: 0.8rem;
     flex-shrink: 0;
+    box-sizing: border-box;
   }
 
-  .square.--_need-border {
-    border: 1px solid var(--color-body--light);
-    box-sizing: border-box;
+  .square.--_white-tone {
+    border: 1px solid light-dark(var(--_black-border), transparent);
+  }
+  .square.--_black-tone {
+    border: 1px solid light-dark(transparent, var(--_white-border));
   }
 </style>
