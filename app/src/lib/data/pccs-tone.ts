526c5e9fab33ae5b7d85011294d09bb882ef1c0a
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-04-14 10:58:35 +0900

    トーンを基準にした配色ルールMapの雛形を作成

---
+++ app/src/lib/data/pccs-tone.ts
@@ -0,0 +1,146 @@
+import pccsTones from "./pccs_tone.json"
+
+export const PCCS_TONES = pccsTones
+
+// 有彩色トーンの一覧
+export const PCCS_CHROMATIC_TONE_SYMBOLS = PCCS_TONES.filter((tone) => !tone.isNeutral).map(
+  (tone) => tone.toneSymbol
+)
+// 無彩色トーンの一覧（グレイ系トーンはGyのみ）
+export const PCCS_ACHROMATIC_TONE_SYMBOLS_MINIMUM = PCCS_TONES.filter((tone) => tone.isNeutral).map(
+  (tone) => tone.toneSymbol
+)
+// 無彩色トーンの一覧（グレイ系トーンを3種類に細分化）
+export const PCCS_ACHROMATIC_TONE_SYMBOLS = ["W", "ltGy", "mGy", "dkGy", "Bk"]
+
+// 低彩度トーンの一覧
+export const PCCS_LOW_SATURATION_TONE_SYMBOLS = ["p", "ltg", "g", "dkg"]
+// 中彩度トーンの一覧
+export const PCCS_MEDIUM_SATURATION_TONE_SYMBOLS = ["lt", "sf", "d", "dk"]
+// 高彩度トーンの一覧
+export const PCCS_HIGH_SATURATION_TONE_SYMBOLS = ["b", "s", "dp", "v"]
+
+// 低彩度トーンかどうか
+export const isLowSaturationTone = (toneSymbol: string) =>
+  PCCS_LOW_SATURATION_TONE_SYMBOLS.includes(toneSymbol)
+// 中彩度トーンかどうか
+export const isMediumSaturationTone = (toneSymbol: string) =>
+  PCCS_MEDIUM_SATURATION_TONE_SYMBOLS.includes(toneSymbol)
+// 高彩度トーンかどうか
+export const isHighSaturationTone = (toneSymbol: string) =>
+  PCCS_HIGH_SATURATION_TONE_SYMBOLS.includes(toneSymbol)
+
+// 明清色トーンの一覧
+export const PCCS_TINT_TONE_SYMBOLS = ["p", "lt", "b"]
+// 暗清色トーンの一覧
+export const PCCS_SHADE_TONE_SYMBOLS = ["dkg", "dk", "dp"]
+
+// 明清色かどうか
+export const isTintTone = (toneSymbol: string) => PCCS_TINT_TONE_SYMBOLS.includes(toneSymbol)
+// 暗清色かどうか
+export const isShadeTone = (toneSymbol: string) => PCCS_SHADE_TONE_SYMBOLS.includes(toneSymbol)
+
+// 対照トーンかどうか（横・縦・斜め方向のいずれでも隣り合っていない）
+export const isContrastingTone = (toneSymbol: string): boolean => {
+  // TODO: 横・縦・斜め方向のいずれでも隣り合っていないトーンを判定できるようにする
+  return false
+}
+
+interface ToneBasedPaletteRule {
+  // ルール名
+  label: string
+  // 初期状態で選択できるトーン
+  allowedTones: string[]
+  // あるトーンを選んだ時に、次にどのトーンを選択可能にするか
+  // どのトーンも選択できない場合はnullを返す
+  suggestNext: (toneSymbol: string) => string[] | null
+}
+
+const PCCS_TONE_BASED_PALETTE_RULE: Record<string, ToneBasedPaletteRule> = {
+  同一トーン: {
+    label: "同一トーン",
+    allowedTones: PCCS_CHROMATIC_TONE_SYMBOLS,
+    // 同一トーンなので他のトーンは選択できない
+    suggestNext: (_tone: string) => null
+  },
+  "類似トーン（縦方向）": {
+    label: "類似トーン（縦方向）",
+    allowedTones: PCCS_CHROMATIC_TONE_SYMBOLS.filter((tone) => tone !== "v"),
+    suggestNext: (tone: string) => {
+      // TODO: 縦に隣り合う全トーンを返すようにする
+      return [tone]
+    }
+  },
+  "類似トーン（横方向）": {
+    label: "類似トーン（横方向）",
+    allowedTones: PCCS_CHROMATIC_TONE_SYMBOLS,
+    suggestNext: (tone: string) => {
+      // TODO: 横に隣り合う全トーンを返すようにする
+      return [tone]
+    }
+  },
+  "類似トーン（斜め方向）": {
+    label: "類似トーン（斜め方向）",
+    allowedTones: PCCS_CHROMATIC_TONE_SYMBOLS,
+    suggestNext: (tone: string) => {
+      // TODO: 斜めに隣り合う全トーンを返すようにする
+      return [tone]
+    }
+  },
+  "対照トーン（明度方向）": {
+    label: "対照トーン（明度方向）",
+    allowedTones: [...PCCS_TINT_TONE_SYMBOLS, ...PCCS_SHADE_TONE_SYMBOLS],
+    suggestNext: (tone: string) => {
+      if (isTintTone(tone)) return PCCS_SHADE_TONE_SYMBOLS
+      if (isShadeTone(tone)) return PCCS_TINT_TONE_SYMBOLS
+      return null
+    }
+  },
+  "対照トーン（彩度方向）": {
+    label: "対照トーン（彩度方向）",
+    allowedTones: [...PCCS_LOW_SATURATION_TONE_SYMBOLS, ...PCCS_HIGH_SATURATION_TONE_SYMBOLS],
+    suggestNext: (tone: string) => {
+      if (isHighSaturationTone(tone)) return PCCS_LOW_SATURATION_TONE_SYMBOLS
+      if (isLowSaturationTone(tone)) return PCCS_HIGH_SATURATION_TONE_SYMBOLS
+      return null
+    }
+  },
+  "対照トーン（明度・彩度方向）": {
+    label: "対照トーン（明度・彩度方向）",
+    allowedTones: PCCS_CHROMATIC_TONE_SYMBOLS.filter((tone) => !["sf", "d"].includes(tone)),
+    suggestNext: (tone: string) => {
+      // TODO: 対照トーン（明度方向）・対照トーン（彩度方向）・類似トーンのいずれにも当てはまらないトーンを返すようにする
+      return [tone]
+    }
+  },
+  無彩色どうし: {
+    label: "無彩色どうし",
+    allowedTones: PCCS_ACHROMATIC_TONE_SYMBOLS,
+    suggestNext: (tone: string) => {
+      // 自分以外の無彩色トーンならなんでもいい
+      return PCCS_ACHROMATIC_TONE_SYMBOLS.filter((t) => t !== tone)
+    }
+  },
+  無彩色と高彩度: {
+    label: "無彩色と高彩度",
+    allowedTones: [...PCCS_ACHROMATIC_TONE_SYMBOLS, ...PCCS_HIGH_SATURATION_TONE_SYMBOLS],
+    suggestNext: (tone: string) => {
+      if (PCCS_ACHROMATIC_TONE_SYMBOLS.includes(tone)) return PCCS_HIGH_SATURATION_TONE_SYMBOLS
+      if (PCCS_HIGH_SATURATION_TONE_SYMBOLS.includes(tone)) return PCCS_ACHROMATIC_TONE_SYMBOLS
+      return null
+    }
+  },
+  無彩色と低彩度: {
+    label: "無彩色と低彩度",
+    allowedTones: [...PCCS_ACHROMATIC_TONE_SYMBOLS, ...PCCS_LOW_SATURATION_TONE_SYMBOLS],
+    suggestNext: (tone: string) => {
+      if (PCCS_ACHROMATIC_TONE_SYMBOLS.includes(tone)) return PCCS_LOW_SATURATION_TONE_SYMBOLS
+      if (PCCS_LOW_SATURATION_TONE_SYMBOLS.includes(tone)) return PCCS_ACHROMATIC_TONE_SYMBOLS
+      return null
+    }
+  }
+}
+
+export const PCCS_TONE_BASED_PALETTE_RULE_MAP = new Map(
+  Object.entries(PCCS_TONE_BASED_PALETTE_RULE)
+)
