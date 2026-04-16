72a814b4ba23f5be2cf467ee91049c8e512d86e9
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-04-17 05:39:45 +0900

    「色相環を分割した配色」追加

--- app/src/lib/demo/color-palette/AnalyzedPalette.svelte
+++ app/src/lib/demo/color-palette/AnalyzedPalette.svelte
@@ -8,9 +8,10 @@
 
   interface Props {
     pccsCSV: string
+    hideTone?: boolean
   }
 
-  let { pccsCSV }: Props = $props()
+  let { pccsCSV, hideTone = false }: Props = $props()
 
   const pccsSymbols = $derived(() => pccsCSV.split(",").map((s) => s.trim()))
 
@@ -27,9 +28,11 @@
 <div class="analyzed-palette-root" class:--_anki={isAnki}>
   <div class="palette-preview"><ColorPalettePreview pccsSymbols={pccsSymbols()} /></div>
   <div class="hue-wheel"><HueWheel displayedPCCSList={displayedPCCSList()} /></div>
-  <div class="tone-diagram">
-    <ToneDiagram displayedPCCSList={displayedPCCSList()} isCard199={false} />
-  </div>
+  {#if !hideTone}
+    <div class="tone-diagram">
+      <ToneDiagram displayedPCCSList={displayedPCCSList()} isCard199={false} />
+    </div>
+  {/if}
 </div>
 
 <style>
