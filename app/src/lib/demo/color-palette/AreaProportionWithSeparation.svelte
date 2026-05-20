954b8e38b0d420671e87ad1b404916a84ca5f49f
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-05-20 11:09:51 +0900

    セパレーション例を示すコンポーネントを実装

---
+++ app/src/lib/demo/color-palette/AreaProportionWithSeparation.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import { PCCS_HEX_MAP } from "$lib/data/pccs"
+
+  interface Props {
+    base: string
+    assort: string
+    separation: string
+    baseRatio?: number
+    assortRatio?: number
+    separationRatio?: number
+    maxW?: number
+  }
+
+  let {
+    base,
+    assort,
+    separation,
+    baseRatio = 5.5,
+    assortRatio = 4.25,
+    separationRatio = 0.25,
+    maxW = 400
+  }: Props = $props()
+
+  const baseColor = $derived(PCCS_HEX_MAP.get(base)!)
+  const assortColor = $derived(PCCS_HEX_MAP.get(assort)!)
+  const separationColor = $derived(PCCS_HEX_MAP.get(separation)!)
+</script>
+
+<div class="rect" style:max-width={maxW + "px"}>
+  <div class="block" style:background-color={baseColor} style:flex-grow={baseRatio}></div>
+  <div
+    class="block"
+    style:background-color={separationColor}
+    style:flex-grow={separationRatio}
+  ></div>
+  <div class="block" style:background-color={assortColor} style:flex-grow={assortRatio}></div>
+</div>
+
+<style>
+  .rect {
+    display: flex;
+    width: 100%;
+    height: 100px;
+    overflow: hidden;
+  }
+
+  .block {
+    height: 100%;
+  }
+</style>
