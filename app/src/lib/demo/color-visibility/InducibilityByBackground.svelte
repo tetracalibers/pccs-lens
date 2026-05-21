c34c7fd8f8a0733d417e04b2a22090477e3cb15e
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-05-21 15:09:27 +0900

    正方形ではなくアイコンに

--- app/src/lib/demo/color-visibility/InducibilityByBackground.svelte
+++ app/src/lib/demo/color-visibility/InducibilityByBackground.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
   import { PCCS_HEX_MAP } from "$lib/data/pccs"
+  import Icon from "@iconify/svelte"
 
   const ROW_ON_WHITE = ["v5", "v8", "v2"]
-  const ROW_ON_BLACK = ["v5", "v2", "v8"]
+  const ROW_ON_BLACK = ["v2", "v5", "v8"]
 </script>
 
 <div class="container">
   <div class="row row--white">
     {#each ROW_ON_WHITE as notation (notation)}
-      <div class="swatch" style:background-color={PCCS_HEX_MAP.get(notation)}></div>
+      <Icon
+        icon="material-symbols-light:warning-rounded"
+        width="62"
+        style="color: {PCCS_HEX_MAP.get(notation)};"
+      ></Icon>
     {/each}
   </div>
   <div class="row row--black">
     {#each ROW_ON_BLACK as notation (notation)}
-      <div class="swatch" style:background-color={PCCS_HEX_MAP.get(notation)}></div>
+      <Icon
+        icon="material-symbols-light:warning-rounded"
+        width="62"
+        style="color: {PCCS_HEX_MAP.get(notation)};"
+      ></Icon>
     {/each}
   </div>
 </div>
@@ -23,15 +32,16 @@
     display: flex;
     flex-direction: column;
     gap: 1rem;
-    padding: 0.5rem 1.25rem;
+    padding: 0.5rem 1rem;
   }
 
   .row {
     display: flex;
     justify-content: center;
-    gap: 1.75rem;
-    padding: 1.25rem;
+    gap: 1.5rem;
+    padding: 1rem;
     border: 1px solid var(--color-body);
+    border-radius: 0.5rem;
   }
 
   .row--white {
@@ -40,11 +50,5 @@
 
   .row--black {
     background-color: #000000;
-  }
-
-  .swatch {
-    width: 3rem;
-    height: 3rem;
-    flex-shrink: 0;
   }
 </style>
