ae2d083fc0a1ff0cb35c7cf62c6b0181fae9001f
tomixy <92695929+tetracalibers@users.noreply.github.com>
2026-05-22 20:07:55 +0900

    Create HexColor.svelte

---
+++ app/src/lib/demo/HexColor.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  interface Props {
+    hex: string
+  }
+
+  let { hex }: Props = $props()
+</script>
+
+<div class="square" style:--_hex-color={hex}></div>
+
+<style>
+  .square {
+    --_square-size: 5rem;
+    --_black-border: rgba(0, 0, 0, 0.2);
+    --_white-border: rgba(255, 255, 255, 0.25);
+
+    width: var(--_square-size);
+    height: var(--_square-size);
+    background-color: var(--_hex-color);
+    flex-shrink: 0;
+    box-sizing: border-box;
+    border: 1px solid light-dark(var(--_black-border), var(--_white-border));
+  }
+</style>
